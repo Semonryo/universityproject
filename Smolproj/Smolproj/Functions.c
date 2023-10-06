287bc4235cc62a585386195daae198878de731e1
Semonryo <145182693+Semonryo@users.noreply.github.com>
2023-10-06 22:37:57 +0300

    Update Functions.c

--- Smolproj/Smolproj/Functions.c
+++ Smolproj/Smolproj/Functions.c
@@ -1,7 +1,23 @@
+#include <stdio.h>
 #include "Functions.h"
+#include "math.h"
 
 unsigned long long Square(const long long value_)
 {
 	
 	return value_ * value_;
+}
+
+bool Prime(unsigned long long int x) 
+{
+	if (x % 2 == 0) 
+		return false;
+
+	for (unsigned int n = 3; n < sqrt((double) x + 1; n += 2) 
+	{
+		if (x % n == 0)
+			return false;
+	}
+
+	return true;
 }
